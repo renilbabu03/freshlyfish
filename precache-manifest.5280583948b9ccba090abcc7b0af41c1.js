8eb913d81c667e28d71c4147966cc88ea333772a
Renil Babu <renil@infospoke.in>
2020-08-27 20:16:23 +0530

    first commit

---
+++ precache-manifest.5280583948b9ccba090abcc7b0af41c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "604992aaf22a99f63044",
+    "url": "/static/js/main.604992aa.chunk.js"
+  },
+  {
+    "revision": "9ce3a5e452ba5ce2cd61",
+    "url": "/static/js/2.9ce3a5e4.chunk.js"
+  },
+  {
+    "revision": "604992aaf22a99f63044",
+    "url": "/static/css/main.cb275996.chunk.css"
+  },
+  {
+    "revision": "9ce3a5e452ba5ce2cd61",
+    "url": "/static/css/2.6cfe898d.chunk.css"
+  },
+  {
+    "revision": "4b18d54581606e82873b2eea0d1538c5",
+    "url": "/index.html"
+  }
+];
